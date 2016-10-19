14e06d2c6c1983eb760d1fb35de8fce4ea91b2f8
Brian Wynn <b_wynn@apple.com>
2016-10-19 12:01:41 -0700

    Defining various app components, router, unit tests written and expounded on.

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -5,7 +5,8 @@ import {
     scryRenderedDOMComponentsWithTag,
     Simulate
     } from 'react-addons-test-utils';
-import Voting from '../../src/components/Voting';
+import {List} from 'immutable';
+import {Voting} from '../../src/components/Voting';
 import {expect} from 'chai';
 
 describe('Voting', () => {
@@ -64,6 +65,26 @@ describe('Voting', () => {
         const winner = ReactDOM.findDOMNode(component.refs.winner);
         expect(winner).to.be.ok;
         expect(winner.textContent).to.contain('Trainspotting');
+    });
+
+    it('renders as a pure component', () => {
+        const pair = ['Trainspotting', '28 Days Later'];
+        const container = document.createElement('div');
+        let component = ReactDOM.render(
+            <Voting pair={pair} />,
+            container
+        );
+
+        let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+        expect(firstButton.textContent).to.equal('Trainspotting');
+
+        pair[0] = 'Sunshine';
+        component = ReactDOM.render(
+            <Voting pair={pair}/>,
+            container
+        );
+        firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+        expect(firstButton.textContent).to.equal('Trainspotting');
     });
 
 });
